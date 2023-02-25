5a5cee2b58e9a886f950e5353971a6f03659bd1e
RandomTreasure <h.p.lovecrafted.11.2@gmail.com>
2023-02-25 15:10:54 +0100

    AddProduct kinda working

--- FoodGuard_1/FoodGuard_1/ViewModels/AddProductPageViewModel.cs
+++ FoodGuard_1/FoodGuard_1/ViewModels/AddProductPageViewModel.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FoodGuard_1.Models;
+using FoodGuard_1.Services;
+using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace FoodGuard_1.ViewModels
 {
     public class AddProductPageViewModel : ViewModelTemplate
     {
+        public string EntryName { get; set; }
+        public string EntryTags { get; set; }
+        public AsyncCommand SaveCommand { get; }
+
+
+        // this stuff here is where the server should actually be called to save some items
+        public DummyData data = new DummyData();
+        public ObservableRangeCollection<FoodItem> dataSample { get; set; }
+        //
+        
+        public AddProductPageViewModel()
+        {
+            
+            SaveCommand = new AsyncCommand(Save);
+
+            // ^
+            dataSample = new ObservableRangeCollection<FoodItem>();
+            dataSample = data.GetData();
+        }
+
+        public async Task Save()
+        {
+            dataSample.Add( new FoodItem { Id = 4, Name = EntryName, Quantity = 1, Tags = EntryTags, ExpiryDate = DateTime.Now});
+
+           await Shell.Current.GoToAsync("..");
+        }
     }
 }
