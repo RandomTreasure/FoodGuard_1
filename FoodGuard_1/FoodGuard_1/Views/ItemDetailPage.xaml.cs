05f94101141bbf4357988b59d83ea86ea0f1ed7a
RandomTreasure <h.p.lovecrafted.11.2@gmail.com>
2023-02-25 09:25:15 +0100

    Add project files.

---
+++ FoodGuard_1/FoodGuard_1/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FoodGuard_1.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace FoodGuard_1.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ FoodGuard_1/FoodGuard_1/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FoodGuard_1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodGuard_1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
