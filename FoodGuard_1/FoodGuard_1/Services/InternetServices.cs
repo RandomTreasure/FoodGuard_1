e838754f0e6b5cd1abd27e8bb5c4bcd728732913
RandomTreasure <h.p.lovecrafted.11.2@gmail.com>
2023-02-27 19:42:09 +0100

    Working internetServices

---
+++ FoodGuard_1/FoodGuard_1/Services/InternetServices.cs
@@ -0,0 +1,61 @@
+﻿using FoodGuard_1.Models;
+using MvvmHelpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FoodGuard_1.Services
+{
+    public class InternetServices
+    {
+        public HttpClient client;
+        public JsonSerializerOptions serializerOptions;
+
+        public List<FoodItem> Items_Web;
+        
+        public InternetServices()
+        {
+            //if (client == null) 
+            //{
+                client = new HttpClient();
+            //}            
+
+            serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+            };
+
+        }
+
+        public async Task<List<FoodItem>> GetItems_Web()
+        {
+            Items_Web = new List<FoodItem>();
+
+            Uri uri = new Uri("http://168.119.117.67:8080/");
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(uri + "getJson");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Items_Web = System.Text.Json.JsonSerializer.Deserialize<List<FoodItem>>(content, serializerOptions);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"\tERROR {0}", ex.Message);
+            }
+
+            return Items_Web;
+        }
+    }
+}
--- FoodGuard_1/FoodGuard_1/ViewModels/RecapPageViewModel.cs
+++ FoodGuard_1/FoodGuard_1/ViewModels/RecapPageViewModel.cs
@@ -1,6 +1,42 @@
-﻿namespace FoodGuard_1.ViewModels
+﻿using FoodGuard_1.Models;
+using FoodGuard_1.Services;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoodGuard_1.ViewModels
 {
     public class RecapPageViewModel : ViewModelTemplate
     {
+        public InternetServices service;
+        public AsyncCommand GetFromWebCommand { get; }
+        public Command TestCommand { get; }
+        public int count = 0;
+
+        public string Text
+        {
+            get => "Bella stringa";
+       //     set => SetProperty(ref Text, value);
+        }
+
+        public RecapPageViewModel()
+        {
+            service = new InternetServices();
+            GetFromWebCommand = new AsyncCommand(GetFromWeb);
+            TestCommand = new Command(Test);
+        }
+
+        public async Task<List<FoodItem>> GetFromWeb()
+        {
+            return await service.GetItems_Web();
+        }
+
+        public void Test()
+        {
+            //Console.WriteLine("Premuto Test Button");
+            count++;
+            //Text = $"Hai premuto il bottone {count} volte";
+        }
     }
 }
